7b0e166e5f350d419933f14a347046230d521c97
Theodore Morley <theodore.j.morley@vumc.org>
2019-12-09 15:10:47 -0600

    Final version

--- pull_broad_table.py
+++ pull_broad_table.py
@@ -19,47 +19,23 @@ if __name__=='__main__':
 
     el = browser.find_element_by_id("table-panel")
 
-    #action = webdriver.common.action_chains.ActionChains(browser)
-    #action.move_to_element_with_offset(el, 100, 100)
-    #action.perform()
-
-    #elem = browser.find_element_by_class_name("grid-row")
-
     r_els=[]
 
     while no_of_pagedowns:
         row_elems=browser.find_elements_by_class_name("grid-row")
         for r in row_elems:
             r_els.append(r.text)
-        div_iv = browser.find_element_by_class_name("grid-row")
+        div_iv = browser.find_elements_by_class_name("grid-row")[-1]
         action = webdriver.common.action_chains.ActionChains(browser)
         action.click(div_iv).send_keys(Keys.PAGE_DOWN).perform()
         time.sleep(.3)
         no_of_pagedowns-=1
     
-    #print(row_elems)
     print(len(r_els))
     for i in range(len(r_els)):
         r_els[i]=r_els[i].split('\n')
-    #print(r_els)
     df = pd.DataFrame(r_els)
-    print(df)
-
-    #print(soup.find_all("div"))
-    #Grid__GridHorizontalViewport-sc7cg1-1 hBvlwL
-
-    #div_headers = soup.find('div', {'class':'Grid__HeaderRow-sc7cg1-2 cHNdtw'})
-    #for head in div_headers:
-    #    print(head)
-
-    #mydivs = soup.find_all("div", {"class":'grid-row'})#{"class": "grid-row grid-row-stripe"})#"Grid__GridHorizontalViewport-sc7cg1-1 hBvlwL"})
-
-    #print(len(mydivs))
-    #print(mydivs)
-    #print(pd.read_html(url))
-
-    #print(len(soup.find_all("table")))
-    #print(soup.find("table", {"id": "expanded_standings"}))
+    df.to_csv(sys.argv[2], index=False)
 
     browser.close()
     browser.quit()
